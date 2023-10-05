f06ea72e929a4010041f57533eb8a9263cb7d7b0
Tkach Anatolii <tkach.vip@gmail.com>
2023-10-05 23:42:21 +0600

    test_dz№5

--- tests/test_dz_selene_8_5.py
+++ tests/test_dz_selene_8_5.py
@@ -10,13 +10,14 @@ def test_tools_qa():
     browser.element('#lastName').type('Tkach')
     browser.element('#userEmail').type('tkach.vip@gmail.com')
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('550391393')
+    browser.element('#userNumber').type('5503913933')
     browser.element('#dateOfBirthInput').click()
     browser.element('[class="react-datepicker__month-select"]').click().element('[value="2"]').click()
     browser.element('[class="react-datepicker__year-select"]').click().element('[value="1999"]').click()
     browser.element('[class="react-datepicker__day react-datepicker__day--008"]').click()
-    browser.element('#subjectsInput').type('automated testing').press_enter()
+    browser.element('#subjectsInput').type('automated testing')
     browser.element('[id="stateCity-label"]').perform(command.js.scroll_into_view)
+    browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('image/kat.png.png'))
     browser.element('[id="currentAddress"]').type('Pushkin 22').perform(command.js.scroll_into_view)
     browser.element('#react-select-3-input').type('NCR').press_enter()
@@ -24,4 +25,14 @@ def test_tools_qa():
     browser.element('[id="submit"]').press_enter()
 
     #проверка резльтата
-    browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
+    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
+    browser.element('.table-responsive').all('tr td:nth-child(2)').should(have.text(
+        'Anatolii Tkach',
+        'tkach.vip@gmail.com',
+        'Male',
+        '5503913933',
+        '08 March 1999',
+        'Sports',
+        'Kat.png.png',
+        'Pushkin 22',
+        'NCR Delhi'))
