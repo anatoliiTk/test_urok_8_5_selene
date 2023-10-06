2b010f35486a1a3ac1769def80778caf84631e3c
Tkach Anatolii <tkach.vip@gmail.com>
2023-10-06 12:10:04 +0600

    test_dz№5

--- tests/test_dz_selene_8_5.py
+++ tests/test_dz_selene_8_5.py
@@ -15,10 +15,10 @@ def test_tools_qa():
     browser.element('[class="react-datepicker__month-select"]').click().element('[value="2"]').click()
     browser.element('[class="react-datepicker__year-select"]').click().element('[value="1999"]').click()
     browser.element('[class="react-datepicker__day react-datepicker__day--008"]').click()
-    browser.element('#subjectsInput').type('automated testing').press_enter()
+    browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element('[id="stateCity-label"]').perform(command.js.scroll_into_view)
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('image/kat.png.png'))
+    browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('image/cat.png'))
     browser.element('[id="currentAddress"]').type('Pushkin 22').perform(command.js.scroll_into_view)
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
@@ -32,8 +32,9 @@ def test_tools_qa():
         'Male',
         '5503913933',
         '08 March,1999',
+        'Maths',
         'Sports',
-        'Kat.png.png',
+        'cat.png',
         'Pushkin 22',
         'NCR Delhi'
     ))
